c8e4f0c9e87f789b79f48dfe7abadbb41c47bed2
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-24 10:32:38 -0500

    add: asgi app in `routing.py`

--- backend/rideshare_app/rideshare_app/routing.py
+++ backend/rideshare_app/rideshare_app/routing.py
@@ -1,12 +1,15 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
+from django.core.asgi import get_asgi_application
 from rideshare_app import middleware
 import app.routing
 import django
 
 django.setup()
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
+    "http": django_asgi_app,
     "websocket": middleware.TokenAuthMiddlewareStack(URLRouter(
         app.routing.websocket_urlpatterns
     ))
