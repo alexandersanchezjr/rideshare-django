35ee430a5339cf4f237be5edc227f21a8b964458
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-24 14:59:19 -0500

    add: sync_to_async functionality to method

--- backend/rideshare_app/rideshare_app/middleware.py
+++ backend/rideshare_app/rideshare_app/middleware.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AnonymousUser
 from django.db import close_old_connections
 from channels.auth import AuthMiddlewareStack
 from rest_framework_simplejwt.tokens import AccessToken
-
+from channels.db import database_sync_to_async
 
 User = get_user_model()
 
@@ -17,7 +17,7 @@ class TokenMiddleware:
     def __init__(self, inner):
         self.inner = inner
 
-    def __call__(self, scope, receive, send):
+    async def __call__(self, scope, receive, send):
         close_old_connections()
         query_strings = parse_qs(scope['query_string'].decode())
         token = query_strings.get('token')
@@ -26,8 +26,10 @@ class TokenMiddleware:
             return self.inner(scope, receive, send)
         try:
             access_token = AccessToken(token[0])
-            user = User.objects.get(id=access_token['id'])
+            user_id = access_token.payload['id']
+            user = await self._get_user_by_id(user_id)
         except Exception as exception:
+            print('Exception: ', exception)
             scope['user'] = AnonymousUser()
             return self.inner(scope, receive, send)
         if not user.is_active:
@@ -36,6 +38,9 @@ class TokenMiddleware:
         scope['user'] = user
         return self.inner(scope, receive, send)
 
+    @database_sync_to_async
+    def _get_user_by_id(self, user_id):
+        return User.objects.get(id=user_id)
 
 def TokenAuthMiddlewareStack(inner):
     """
