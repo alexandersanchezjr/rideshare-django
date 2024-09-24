76dd1974107176a20a292d7d860e3986aee3c850
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-23 23:50:40 -0500

    add: `AUTH_USER_MODEL` for model usage

--- backend/rideshare_app/rideshare_app/settings.py
+++ backend/rideshare_app/rideshare_app/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'app',
 ]
 
+AUTH_USER_MODEL = 'app.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
