b298fca2ce225a9f4dd6b1fdb82ec092d4368770
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-23 23:33:31 -0500

    create: django project

---
+++ backend/rideshare_app/rideshare_app/asgi.py
@@ -0,0 +1,29 @@
+"""
+ASGI config for rideshare_app project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rideshare_app.settings')
+
+django_asgi_application = get_asgi_application()
+
+import app.routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_application,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            app.routing.websocket_urlpatterns
+        )
+    ),
+})
