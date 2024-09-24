6e0e5f7f43c0743c34fcb0b55ba248408d571f66
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-23 23:51:17 -0500

    change: urls.py

--- backend/rideshare_app/rideshare_app/urls.py
+++ backend/rideshare_app/rideshare_app/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("ws/", include("app.urls")),
+    path('', include("app.urls")),
     path('admin/', admin.site.urls),
 ]
