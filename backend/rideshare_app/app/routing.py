ea41010d1810d22c39478db14e6bb006c2901bd1
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-24 01:24:32 -0500

    change: RideConsumer syntax

--- backend/rideshare_app/app/routing.py
+++ backend/rideshare_app/app/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/rideshare/$', consumers.RideConsumer.as_asgi()),
+    re_path(r'ws/rideshare/$', consumers.RideConsumer),
 ]
