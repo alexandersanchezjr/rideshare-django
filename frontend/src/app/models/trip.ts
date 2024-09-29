cfcafdf3e52b88c6a11694f5a23aaff34de08b02
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 00:56:37 -0500

    add: api response type

--- frontend/src/app/models/trip.ts
+++ frontend/src/app/models/trip.ts
@@ -1,21 +1,42 @@
-import { User } from './user';
+import { User, UserResponse } from './user';
 
 export class Trip {
-  constructor (
-    public id: string,
-    public created: string,
-    public updated: string,
-    public pickUpAddress: string,
-    public dropOffAddress: string,
-    public status: Status,
-    public driver: User,
-    public rider: User,
-  ) {}
+
+  public id: string;
+  public created: string;
+  public updated: string;
+  public pickUpAddress: string;
+  public dropOffAddress: string;
+  public status: Status;
+  public driver: User;
+  public rider: User;
+
+  constructor (tripResponse: TripResponse) {
+    this.id = tripResponse.id;
+    this.created = tripResponse.created;
+    this.updated = tripResponse.updated;
+    this.pickUpAddress = tripResponse.pickUpAddress;
+    this.dropOffAddress = tripResponse.dropOffAddress;
+    this.status = tripResponse.status as Status;
+    this.driver = new User(tripResponse.driver);
+    this.rider = new User(tripResponse.rider);
+  }
 }
 
-const enum Status {
+export const enum Status {
   REQUESTED = 'REQUESTED',
   STARTED = 'STARTED',
   IN_PROGRESS = 'IN_PROGRESS',
   COMPLETED = 'COMPLETED',
+}
+
+export type TripResponse = {
+  id: string;
+  created: string;
+  updated: string;
+  pickUpAddress: string;
+  dropOffAddress: string;
+  status: string;
+  driver: UserResponse;
+  rider: UserResponse;
 }
--- frontend/src/app/models/user.ts
+++ frontend/src/app/models/user.ts
@@ -1,13 +1,21 @@
 export class User {
-  constructor(
-    public id: number,
-    public username: string,
-    public firstname: string,
-    public lastname: string,
-    private group: UserGroup,
-    public photo: string,
-    public password?: string,
-  ) {}
+  public id: number;
+  public username: string;
+  public firstname: string;
+  public lastname: string;
+  public group: UserGroup;
+  public photo: string;
+  public password?: string;
+
+  constructor(userResponse: UserResponse) {
+    this.id = userResponse.id;
+    this.username = userResponse.username;
+    this.firstname = userResponse.firstname;
+    this.lastname = userResponse.lastname;
+    this.group = userResponse.group === UserGroup.DRIVER ? UserGroup.DRIVER : UserGroup.RIDER;
+    this.photo = userResponse.photo;
+    this.password = userResponse.password
+  }
 
   get isDriver(): boolean {
     return this.group === UserGroup.DRIVER;
@@ -18,7 +26,17 @@ export class User {
   }
 }
 
-export const enum UserGroup {
+export enum UserGroup {
   RIDER = 'rider',
   DRIVER = 'driver',
+}
+
+export type UserResponse = {
+  id: number,
+  username: string,
+  firstname: string,
+  lastname: string,
+  group: string,
+  photo: string,
+  password?: string
 }
