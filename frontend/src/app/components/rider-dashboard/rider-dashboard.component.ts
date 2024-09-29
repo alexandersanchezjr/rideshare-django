38a94975626a2072c3b86ae6ee8019c4d5528ff6
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 18:05:19 -0500

    add: layout and methods to rider components

--- frontend/src/app/components/rider-dashboard/rider-dashboard.component.ts
+++ frontend/src/app/components/rider-dashboard/rider-dashboard.component.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { TripCardComponent } from '@components/trip-card/trip-card.component';
+import { Status, Trip, TripResponse } from '@models/trip';
+import { TripService } from '@services/trip.service';
+import { ToastrService } from 'ngx-toastr';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rider-dashboard',
   standalone: true,
-  imports: [],
+  imports: [TripCardComponent],
   templateUrl: './rider-dashboard.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RiderDashboardComponent {
+  messages!: Subscription;
+  trips!: Trip[];
 
+  constructor(
+    private toastr: ToastrService,
+    private route: ActivatedRoute,
+    private tripService: TripService
+  ) {}
+
+  get currentTrips(): Trip[] {
+    return this.trips.filter(trip => {
+      return trip.driver !== null && trip.status !== Status.COMPLETED;
+    });
+  }
+
+  get completedTrips(): Trip[] {
+    return this.trips.filter(trip => {
+      return trip.status === Status.COMPLETED;
+    });
+  }
+
+  ngOnInit(): void {
+    this.route.data.subscribe({
+      next: (data: Data) => {
+        const tripsResponse: TripResponse[] = data['trips'];
+        this.trips = tripsResponse.map(
+          (tripResponse: TripResponse) => new Trip(tripResponse)
+        );
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+    this.tripService.connect();
+    this.messages = this.tripService.messages.subscribe((message: any) => {
+      const tripResponse: TripResponse = message.data;
+      const trip: Trip = new Trip(tripResponse);
+      this.updateTrips(trip);
+      this.updateToast(trip);
+    });
+  }
+
+  updateTrips(trip: Trip): void {
+    this.trips = this.trips.filter((thisTrip: Trip) => thisTrip.id !== trip.id);
+    this.trips.push(trip);
+  }
+
+  updateToast(trip: Trip): void {
+    if (trip.status === Status.STARTED) {
+      this.toastr.info(`Driver ${trip.driver.username} is coming to pick you up.`);
+    } else if (trip.status === Status.IN_PROGRESS) {
+      this.toastr.info(`Driver ${trip.driver.username} is headed to your destination.`);
+    } else if (trip.status === Status.COMPLETED) {
+      this.toastr.info(`Driver ${trip.driver.username} has dropped you off.`);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.messages.unsubscribe();
+  }
 }
--- frontend/src/app/components/rider-detail/rider-detail.component.ts
+++ frontend/src/app/components/rider-detail/rider-detail.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Data, RouterModule } from '@angular/router';
+import { Trip, TripResponse } from '@models/trip';
 
 @Component({
   selector: 'app-rider-detail',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule],
   templateUrl: './rider-detail.component.html',
   styles: ``
 })
 export class RiderDetailComponent {
+  trip!: Trip;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.data.subscribe({
+      next: (data: Data) => {
+        const tripResponse: TripResponse = data['trip'];
+        this.trip = new Trip(tripResponse);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
