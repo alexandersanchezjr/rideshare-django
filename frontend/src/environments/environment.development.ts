df8d03b13d6f3fa2a556bc83837d03c4c6454b85
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-28 22:55:08 -0500

    add: useful endpoints

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'http://localhost:8000/',
+  signup: 'v1/signup',
+  login: 'v1/login',
+  tokenRefresh: 'v1/token/refresh',
+  trips: 'v1/trip',
   googleMapsApiKey: '',
 };
