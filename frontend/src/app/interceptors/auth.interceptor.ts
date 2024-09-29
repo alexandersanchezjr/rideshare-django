872b9411935d0e4bf6e12463a3ff4dea4c7a6e44
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 01:14:55 -0500

    add: token to request in header

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,6 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '@services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  
+  const authService: AuthService = inject(AuthService);
+  const token = authService.accessToken;
+
+  if (token) {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+  }
+
   return next(req);
 };
