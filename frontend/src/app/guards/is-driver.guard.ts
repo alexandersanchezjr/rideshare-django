60bf9c236f9dfbf87090a51692a05358c8347f33
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 01:08:35 -0500

    add: AuthService to guards

--- frontend/src/app/guards/is-driver.guard.ts
+++ frontend/src/app/guards/is-driver.guard.ts
@@ -1,5 +1,10 @@
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '@services/auth.service';
+import { User } from '@models/user';
 
 export const isDriverGuard: CanActivateFn = (route, state) => {
-  return true;
+  const authService = inject(AuthService);
+  const user: User | undefined = authService.user;
+  return user ? user.isDriver : false;
 };
