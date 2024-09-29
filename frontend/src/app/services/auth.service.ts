e9f05e304f8ae96d1d79e85cf3597b0a0a985221
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-28 23:06:58 -0500

    add: mehotds and attributes

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,9 +1,66 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'environment'
+import { User, UserGroup } from '@models/user';
+
+type Token = {
+  access: string;
+  refresh: string;
+};
+
+type LoginData = {
+  username: string;
+  password: string;
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private readonly apiUrl = environment.apiUrl;
+  private readonly loginUrl = this.apiUrl + environment.login;
+  private readonly signupUrl = this.apiUrl + environment.signup;
+
+  constructor(private http: HttpClient) { }
+
+  login (data: LoginData): Observable<Token> {
+    return this.http.post<Token>(this.loginUrl, data).pipe(
+      tap(token => localStorage.setItem('rideshare.auth', JSON.stringify(token)))
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('rideshare.auth');
+  }
+
+  get accessToken(): string {
+    const token = localStorage.getItem('rideshare.auth');
+    return token ? JSON.parse(token).access : '';
+  }
+
+  signup (data: User): Observable<User> {
+    const formData = new FormData();
+    formData.append('username', data.username);
+    formData.append('password1', data.password!);
+    formData.append('password2', data.password!);
+    formData.append('firstname', data.firstname);
+    formData.append('lastname', data.lastname);
+    formData.append('photo', data.photo);
+    formData.append('group', data.isDriver ? UserGroup.DRIVER : UserGroup.RIDER);
+
+    return this.http.post<User>(this.signupUrl, formData);
+  }
+
+  get user(): User | undefined {
+    const accessToken = this.accessToken;
+    return accessToken ? this.getUserFromAccessToken(accessToken) : undefined;
+  }
+
+  private getUserFromAccessToken(accessToken: string): User {
+    const payload: string = accessToken.split('.')[1];
+    const user: User = JSON.parse(atob(payload));
+    return user;
+  }
 }
