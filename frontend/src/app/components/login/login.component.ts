9a222399079b9f40ef6fbcad7f330c56db91e9a5
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 12:39:15 -0500

    add: layout and methods to login component

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LoginComponent {
+  form: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  constructor(private router: Router, private auth: AuthService) {}
+
+  onSubmit(): void {
+    console.log('Login form submitted');
+    console.log;
+    if (this.form.invalid) return;
+
+    this.auth.login(this.form.value).subscribe({
+      next: () => {
+        console.log('Login successful');
+        this.router.navigate(['']);
+      },
+      error: (error) => {
+        console.error(error);
+      },
+    });
+  }
 }
