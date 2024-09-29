ca0af5034e759abc02eb8867012a9dbed04ae64c
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 01:54:10 -0500

    add: `HomeComponent` layout and styles

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,28 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { User } from '@models/user';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ NgIf, RouterModule],
   templateUrl: './home.component.html',
   styles: ``
 })
 export class HomeComponent {
+  constructor(private auth: AuthService) { }
 
+  get user(): User | undefined {
+    return this.auth.user;
+  }
+
+  get isRider(): boolean {
+    return this.user?.isRider || false;
+  }
+
+  logout(): void {
+    this.auth.logout();
+  }
 }
