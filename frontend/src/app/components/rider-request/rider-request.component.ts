06c77eac11adac7270c3f243d34aee6a30e86f9b
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 18:01:44 -0500

    add: layout, methods and google-maps package to rider-request component

--- frontend/src/app/components/rider-request/rider-request.component.ts
+++ frontend/src/app/components/rider-request/rider-request.component.ts
@@ -1,12 +1,110 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges, ViewChild } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { Trip } from '@models/trip';
+import { AuthService } from '@services/auth.service';
+import { TripService } from '@services/trip.service';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { GoogleMapsModule, GoogleMap } from '@angular/google-maps';
+import { GmapsService } from '@services/gmaps.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-rider-request',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    GoogleMapsModule,
+    RouterModule,
+    NgIf,
+    AsyncPipe,
+  ],
   templateUrl: './rider-request.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RiderRequestComponent {
+  apiLoaded: Observable<boolean>;
+  trip!: Trip;
+  origin!: google.maps.LatLngLiteral;
+  destination!: google.maps.LatLngLiteral;
+  directions!: Observable<google.maps.DirectionsResult | undefined>;
 
+  @ViewChild(GoogleMap)
+  map!: GoogleMap;
+
+  form: FormGroup = new FormGroup({
+    pickUpAddress: new FormControl('', [Validators.required]),
+    dropOffAddress: new FormControl('', [Validators.required]),
+  });
+
+  constructor(
+    private googleMapsService: GmapsService,
+    private router: Router,
+    private toastr: ToastrService,
+    private tripService: TripService,
+    private auth: AuthService
+  ) {
+    this.apiLoaded = googleMapsService.apiLoaded;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['origin'].currentValue) {
+      this.updateMap();
+    }
+  }
+
+  ngOnInit(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.origin = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+
+    this.trip = new Trip({
+      pickUpAddress: this.form.value.pickUpAddress,
+      dropOffAddress: this.form.value.dropOffAddress,
+      rider: this.auth.user!,
+    })
+
+    this.tripService.createTrip(this.trip);
+    this.router.navigateByUrl('/rider');
+  }
+
+  onUpdate(): void {
+    const { pickUpAddress, dropOffAddress } = this.trip;
+    if (pickUpAddress && dropOffAddress) {
+      this.toastr.info('Updating map...');
+      this.directions = this.googleMapsService.directions(
+        pickUpAddress,
+        dropOffAddress
+      );
+    }
+  }
+
+  updateMap(): void {
+    const bounds = new google.maps.LatLngBounds();
+    if (this.origin) {
+      bounds.extend(new google.maps.LatLng(this.origin.lat, this.origin.lng));
+    }
+    if (this.destination) {
+      bounds.extend(
+        new google.maps.LatLng(this.destination.lat, this.destination.lng)
+      );
+    }
+    this.map.fitBounds(bounds);
+    this.map.panToBounds(bounds);
+  }
 }
--- frontend/src/app/services/gmaps.service.ts
+++ frontend/src/app/services/gmaps.service.ts
@@ -1,9 +1,46 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environment';
+import { Observable, map, catchError, of, shareReplay } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GmapsService {
+  apiLoaded: Observable<boolean>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.apiLoaded = http
+      .jsonp(
+        `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}`,
+        'callback'
+      )
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+        shareReplay()
+      );
+  }
+
+  directions(
+    origin: string,
+    destination: string
+  ): Observable<google.maps.DirectionsResult> {
+    const request: google.maps.DirectionsRequest = {
+      origin,
+      destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    };
+    const directionsService = new google.maps.DirectionsService();
+    return new Observable((observer) => {
+      directionsService.route(request, (result, status) => {
+        if (status === 'OK' && result) {
+          observer.next(result);
+        } else {
+          observer.error('Enter two valid addresses.');
+        }
+        observer.complete();
+      });
+    });
+  }
 }
