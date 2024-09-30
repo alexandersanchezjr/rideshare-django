72d8221b97fbd10a721392c417d4b57933e63d25
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 23:05:08 -0500

    add: refresh token

--- frontend/src/app/interceptors/auth.interceptor.ts
+++ frontend/src/app/interceptors/auth.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '@services/auth.service';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService: AuthService = inject(AuthService);
@@ -14,5 +15,31 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+        // Attempt to refresh the token
+        return authService.refreshToken().pipe(
+          switchMap(() => {
+            // After refreshing the token, retry the original request with the new token
+            return next(
+              req.clone({
+                setHeaders: {
+                  Authorization: `Bearer ${authService.accessToken}`,
+                },
+              })
+            );
+          }),
+          catchError((refreshError) => {
+            // Handle token refresh error (e.g., logout the user)
+            authService.logout();
+            return throwError(() => refreshError); // Properly propagate the error
+          })
+        );
+      } else {
+        // For other errors, just propagate the original error
+        return throwError(() => error);
+      }
+    })
+  );
 };
