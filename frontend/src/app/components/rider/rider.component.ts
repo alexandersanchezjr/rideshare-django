a3b05d30557d599cd1343b4d16628d84d0e4d9c8
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-29 23:00:23 -0500

    add: layout for rider component

--- frontend/src/app/components/rider/rider.component.ts
+++ frontend/src/app/components/rider/rider.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-rider',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './rider.component.html',
   styles: ``
 })
