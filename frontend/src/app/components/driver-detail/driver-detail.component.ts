cf26a75d6c322d0c70c6ecaa871e06e7e635b628
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2024-09-30 11:09:11 -0500

    change: `TripDataMessage` type

--- frontend/src/app/components/driver-detail/driver-detail.component.ts
+++ frontend/src/app/components/driver-detail/driver-detail.component.ts
@@ -1,7 +1,7 @@
 import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Data, RouterModule } from '@angular/router';
-import { Trip, Status } from '@models/trip';
+import { Trip, Status, TripDataMessage } from '@models/trip';
 import { AuthService } from '@services/auth.service';
 import { TripService } from '@services/trip.service';
 
@@ -14,6 +14,7 @@ import { TripService } from '@services/trip.service';
 })
 export class DriverDetailComponent {
   trip!: Trip;
+  messageTrip!: TripDataMessage;
   status = Status;
   readonly photo: string = 'https://preview.redd.it/lpdwn1nqd2x81.png?width=640&crop=smart&auto=webp&s=3f91b1b8951078c6ca1de49c4281ecbf2256f6bd';
 
@@ -38,6 +39,14 @@ export class DriverDetailComponent {
   updateTripStatus(status: string): void {
     this.trip.driver = this.auth.user!;
     this.trip.status = status as Status;
-    this.tripService.updateTrip(this.trip);
+
+    this.messageTrip = {
+      id: this.trip.id!,
+      status: this.trip.status,
+      pick_up_address: this.trip.pickUpAddress,
+      drop_off_address: this.trip.dropOffAddress,
+      driver: this.auth.user!.id!,
+    };
+    this.tripService.updateTrip(this.messageTrip);
   }
 }
